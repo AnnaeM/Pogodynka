62caa3f03a91143df2eab2685b5776ad625a4ab3
AnnaeM <anna.d.mazur@gmail.com>
2013-11-16 21:23:38 +0100

    Dzialaj!

---
+++ src/projekt/pogodynkatab/MainActivity.java
@@ -0,0 +1,183 @@
+package projekt.pogodynkatab;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements LocationListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void doPogody(View view) {
+		Intent intent = new Intent(this, ForecastActivity.class);
+		EditText editText = (EditText) findViewById(R.id.miasto);
+		String message = editText.getText().toString();
+		// getWeather gw = new getWeather(message, "Poland");
+		intent.putExtra("Lokacja", message);
+		// intent
+		startActivity(intent);
+	}
+
+	public void test(View view) {
+		Intent intent = new Intent(this, Test.class);
+		startActivity(intent);
+	}
+
+	public void GPS(View view) {
+		uzyjGPS();
+	}
+	
+	public void uzyjGPS() {
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		if (!locationManager
+				.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("GPS nie jest w³¹czony")
+					.setMessage("Czy chcesz przejœæ do ustawieñ GPS?")
+					.setCancelable(true)
+					.setPositiveButton("Tak",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int id) {
+									// TODO Auto-generated method stub
+									startActivity(new Intent(
+											Settings.ACTION_SECURITY_SETTINGS));
+								}
+							})
+					.setNegativeButton("Nie",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int id) {
+									// TODO Auto-generated method stub
+									dialog.cancel();
+									//finish();
+								}
+							});
+
+			AlertDialog alert = builder.create();
+			alert.show();
+
+		}
+
+		else {
+
+		/*	getSystemService(Context.LOCATION_SERVICE);
+			locationManager.requestLocationUpdates(
+					LocationManager.GPS_PROVIDER, 0, 0, this);
+		 	*/
+			Location location = null;
+			
+			getSystemService(Context.LOCATION_SERVICE);
+			locationManager.requestLocationUpdates(
+					LocationManager.GPS_PROVIDER, 0, 0, this);
+			
+			int i = 0;
+			while ((location == null) && (i <= 100)) {
+			//while (location == null) {
+				
+				location = locationManager
+						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+				i++;
+
+			}
+
+			//obs³uga b³êdu GPS
+			if (i >= 100) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Przepraszamy!")
+						.setMessage("Nie uda³o siê pobraæ danych z GPS.")
+						.setPositiveButton("OK",
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// TODO Auto-generated method stub
+										dialog.cancel();
+									}
+								});
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+
+			else {
+				double szerokosc = (double) (location.getLatitude());
+				double dlugosc = (double) (location.getLongitude());
+
+			//	locationManager.removeUpdates(LocationManager.GPS_PROVIDER);
+			//	locationManager.removeUpdates(listener);
+				
+				doPogody(dlugosc, szerokosc);
+			}
+					
+		}
+	}
+
+	public void doPogody(double dlugosc, double szerokosc) {
+		Intent intent = new Intent(this, ForecastActivity.class);
+
+		String dl = String.format("%.4f", dlugosc);
+		String szer = String.format("%.4f", szerokosc);
+
+		dl = dl.replace(',', '.');
+		szer = szer.replace(',', '.');
+
+		String message = szer + "," + dl;
+
+		intent.putExtra("Lokacja", message);
+		startActivity(intent);
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
---
+++ src/projekt/pogodynkatab/Test.java
@@ -0,0 +1,105 @@
+package projekt.pogodynkatab;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class Test extends Activity {
+
+	public JSONObject testowy = new JSONObject();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.test, menu);
+		return true;
+	}
+	
+	public void sporty(View view) {
+
+		try {
+			wlozDane();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Intent intent = new Intent(this, Sporty.class);
+		intent.putExtra("Pogoda", testowy.toString());
+		
+		startActivity(intent);
+
+	}
+	
+	public void ubrania(View view){
+		
+		try {
+			wlozDane();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Intent intent = new Intent(this, Ubrania.class);
+		intent.putExtra("Pogoda", testowy.toString());
+		
+		startActivity(intent);
+		
+	}
+
+	public void wlozDane() throws JSONException {
+
+		EditText miasto = (EditText) findViewById(R.id.miasto);
+		EditText temp = (EditText) findViewById(R.id.temperatura);
+		EditText pogoda = (EditText) findViewById(R.id.pogoda);
+		EditText wiatr = (EditText) findViewById(R.id.wiatr);
+
+		EditText dzienTygodnia = (EditText) findViewById(R.id.dzienTygodnia);
+		EditText dzien = (EditText) findViewById(R.id.dzien);
+		EditText miesiac = (EditText) findViewById(R.id.miesiac);
+		EditText rok = (EditText) findViewById(R.id.rok);
+		EditText godzina = (EditText) findViewById(R.id.godzina);
+		EditText minuty = (EditText) findViewById(R.id.minuty);
+
+		
+		testowy.put("city", miasto.getText().toString());
+
+		// String message = "Mon, 11 Nov 2014 12:00:00 +0100"; 	
+		
+		testowy.put("day", dzien.getText());
+		testowy.put("month", miesiac.getText());
+		testowy.put("hour", godzina.getText());
+		testowy.put("min", minuty.getText());
+		testowy.put("weekDay", dzienTygodnia.getText().toString());
+		testowy.put("city", miasto.getText().toString());
+		testowy.put("latitude", "51.22000122");
+		testowy.put("longitude","22.39999962");
+		testowy.put("feelslike_c", temp.getText());
+		testowy.put("weather", pogoda.getText().toString());
+		
+		if(wiatr.getText()==null){
+			testowy.put("wind_mph", 0);
+		}
+		else
+			testowy.put("wind_mph", wiatr.getText());
+
+
+	}
+
+	
+
+	
+}
+
